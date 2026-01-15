a5233fc651db55b53b48cd6ea25681ffb48099ba
周明 <zhouming@mgtv.com>
2026-01-15 09:38:37 +0800

    Initial Commit

---
+++ MasonryDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasonryDemo
+//
+//  Created by 周明 on 2026/1/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MasonryDemoTests/MasonryDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MasonryDemoTests.swift
+//  MasonryDemoTests
+//
+//  Created by 周明 on 2026/1/15.
+//
+
+import Testing
+@testable import MasonryDemo
+
+struct MasonryDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
